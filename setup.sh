bd699ea9cb39802dfe7169ce568f45b80c0735d8
alchemi5t <soundcrane@gmail.com>
2020-10-11 21:43:24 +0530

    Update setup.sh
    setting up ped2

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 apt-get install libglib2.0-0 -y
 apt install libgl1-mesa-glx -y
+apt install wget
 pip install --upgrade pip 
 pip install matplotlib --ignore-installed certifi
 pip install opencv-python
 pip install scikit-learn
 pip install wandb
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1_oHxf3VaI1Xt1aOUzAnCa180Ha-uYSpl' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1_oHxf3VaI1Xt1aOUzAnCa180Ha-uYSpl" -O ped2.tar.gz && rm -rf /tmp/cookies.txt
+tar -xzvf ped2.tar.gz
