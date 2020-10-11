0a630586a31ca67714248c046ed5f86cfe6a7369
alchemi5t <soundcrane@gmail.com>
2020-10-11 21:36:39 +0530

    Update setup.sh
    upgrade pip and matplotlib

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 apt-get install libglib2.0-0 -y
 apt install libgl1-mesa-glx -y
-pip install matplotlib --ignore-certifi
+pip install --upgrade pip 
+pip install matplotlib --ignore-installed certifi
 pip install opencv-python
 pip install scikit-learn
 pip install wandb
