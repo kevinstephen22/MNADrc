7515fdbc6b7fb8338de9bcdd0651da5cf3b1e9be
alchemi5t <soundcrane@gmail.com>
2020-10-14 01:02:42 +0530

    Update setup.sh
    shanghai tech dataset

--- setup.sh
+++ setup.sh
@@ -7,7 +7,11 @@ pip install opencv-python
 pip install scikit-learn
 pip install wandb
 pip install gdown
-gdown https://drive.google.com/uc?id=17dvOb-t3fIfV1--BW83z1fYaATC9L7hF
-tar -xzvf avenue.tar.gz
+
+gdown https://drive.google.com/uc?id=1EihlS-KNayQPr1VcYmWsOYFtooorNHBc
+tar -xzvf shanghaitech.tar.gz
+
+#gdown https://drive.google.com/uc?id=17dvOb-t3fIfV1--BW83z1fYaATC9L7hF
+#tar -xzvf avenue.tar.gz
 #wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1_oHxf3VaI1Xt1aOUzAnCa180Ha-uYSpl' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1_oHxf3VaI1Xt1aOUzAnCa180Ha-uYSpl" -O ped2.tar.gz && rm -rf /tmp/cookies.txt
 #tar -xzvf ped2.tar.gz
