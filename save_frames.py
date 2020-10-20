69f0c519ea67fbd7b4218c4b9ffdfc43a07fb3a7
alchemi5t <soundcrane@gmail.com>
2020-10-20 16:59:47 +0530

    Update save_frames.py
    optimized for space

--- save_frames.py
+++ save_frames.py
@@ -7,20 +7,24 @@ directory = sys.argv[1]
 
 def mk_frames(vid):
         print(vid)
-        os.mkdir("/".join(vid.split("/")[:-1])+'/frames/'+vid[:-4])
-        cap = cv2.VideoCapture(vid)
+        if(vid[-3:] != "avi"):
+            return
+        global directory
+        os.mkdir(directory+'frames/'+vid[:-4])
+
+        cap = cv2.VideoCapture(directory+"/"+vid)
         i = 0
         ret = True
         while(cap.isOpened()):
                 ret, frame = cap.read()
                 if ret == False:
                         break
-                cv2.imwrite('frames'+'/'+vid[:-4]+'/'+str(i)+'.jpg',frame)
+                cv2.imwrite(directory+'frames'+'/'+vid[:-4]+'/'+str(i)+'.jpg',frame)
                 i+=1
-	 
+
         cap.release()
         cv2.destroyAllWindows()
-
+        os.remove(directory+"/"+vid)
 
 p = Pool(int(sys.argv[2]))
 file_list = os.listdir(directory)
