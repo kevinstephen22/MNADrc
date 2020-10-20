5dc63b92721f649554430579e886656f83607ef8
alchemi5t <soundcrane@gmail.com>
2020-10-20 16:02:14 +0530

    Update save_frames.py
    multiproc for frame gen, Shanghai tech

--- save_frames.py
+++ save_frames.py
@@ -1,19 +1,27 @@
 import cv2
 import os
+from multiprocessing import Pool
+import sys
 
-directory = r'C:\Users\kevin\OneDrive\Desktop\training\videos'
-for filename in os.listdir(directory):
-        print(filename)
-        os.mkdir(directory+'/frames/'+filename)
-        cap = cv2.VideoCapture(filename)
+directory = sys.argv[1]
+
+def mk_frames(vid):
+        print(vid)
+        os.mkdir("/".join(vid.split("/")[:-1])+'/frames/'+vid[:-4])
+        cap = cv2.VideoCapture(vid)
         i = 0
         ret = True
         while(cap.isOpened()):
                 ret, frame = cap.read()
                 if ret == False:
                         break
-                cv2.imwrite('frames'+'/'+filename+'/'+str(i)+'.jpg',frame)
+                cv2.imwrite('frames'+'/'+vid[:-4]+'/'+str(i)+'.jpg',frame)
                 i+=1
 	 
         cap.release()
         cv2.destroyAllWindows()
+
+
+p = Pool(int(sys.argv[2]))
+file_list = os.listdir(directory)
+p.map(mk_frames,file_list)
