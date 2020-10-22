532a1485f57dfb906cc69dfa93423d22277c4909
alchemi5t <soundcrane@gmail.com>
2020-10-22 16:13:26 +0530

    Update Evaluate_Pred_wo_mem.py

--- Evaluate_Pred_wo_mem.py
+++ Evaluate_Pred_wo_mem.py
@@ -87,7 +87,7 @@ model.cuda()
 
 
 labels = np.load('./data/frame_labels_'+args.dataset_type+'.npy')
-if args.dataset_type == 'shanghai':
+if args.dataset_type == 'shanghaitech':
     labels = np.expand_dims(labels, 0)
 
 videos = OrderedDict()
